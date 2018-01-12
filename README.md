4315f325b769685dba2e4e5d3175de6cb3954861
Marek Sirovy <msirovy@gmail.com>
2018-01-12 14:36:01 +0100

    resuscitujeme

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ necestovka
 ==========
 
 
-Ukazka jedne z poslednich Django aplikaci co jsem delal, je to jen naprototypovana verze bez api atp, spis pro predstavu jak pisu kod
 
+objednavka
+----------
 
+naparovat letenku pod uzivatele aby slo tisknout letenky pro 
--- necestovka/views.py
+++ necestovka/views.py
@@ -1,12 +1,10 @@
 from django.http import HttpResponse
 import json
-from pprint import pprint
 
 def index(request):
     """ Index page
     """
     jsondata = json.dumps({'k1':'val1'})
-    #pprint(request)
     return HttpResponse(jsondata, 
                         content_type="application/json")
-                
+
